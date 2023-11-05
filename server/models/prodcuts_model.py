3ca720c78a74b9a8039969ccbbf45aa451bd81ee
EmmaRogel <132635424+EmmaRogel@users.noreply.github.com>
2023-11-05 17:19:34 -0600

    products

--- server/models/prodcuts_model.py
+++ server/models/prodcuts_model.py
@@ -2,17 +2,27 @@ from typing import Optional
 from pydantic import BaseModel
 
 
+class MateriaPrima(BaseModel):
+    _id: Optional[str]
+    tipo: str
+    quantity: int
+    min_stock: int
+    max_stock: int
 class Prducto(BaseModel):
     _id: Optional[str]
-    name: str
+    name_prod: str
     Descripcion: str
     min_stock: int
     max_stock: int
     precio_uni: int
     num_pieza: str
-    mp: list
-    category: str
-    cantidad: int
+    category_prod: str
+    cantidad_prod: int
+    mp: list[MateriaPrima]
+    lote_prod: list
+    lote_alamacen: list
+    class Config:
+        orm_mode = True
 
 
 class updatePrducto(BaseModel):
@@ -23,6 +33,11 @@ class updatePrducto(BaseModel):
     max_stock: Optional[int]
     precio_uni: Optional[int]
     num_pieza: Optional[str]
+    category_prod: Optional[str]
+    cantidad_prod: Optional[int]
     mp: Optional[list]
-    category: Optional[str]
-    cantidad: Optional[int]
+    lote_prod: Optional[list]
+    lote_alamacen: Optional[list]
+    
+
+   
--- server/schemas/schema_prducts.py
+++ server/schemas/schema_prducts.py
@@ -7,9 +7,35 @@ def productEntity(item) -> dict:
         "max_stock": item["max_stock"],
         "precio_uni": item["precio_uni"],
         "num_pieza": item["num_pieza"],
-        "mp": item["mp"],
-        "category": item["category"],
-        "cantidad": item["cantidad"]
+        "category_prod": item["category_prod"],
+        "cantidad_prod": item["catindad_prod"],
+        "mp":item[{
+            "tipo":item["tipo"],
+            "quantity":item["quantity"]
+        }
+        ],
+        "lote_prod": [{
+            "fecha":item["fecha"],
+            "mat_pro":item["mat_prod"]
+        }
+        ],
+        "lote_Almacen": [{
+            "tipo":item["tipo"],
+            "quantity":item["quantity"],
+            "piso":[{
+                "num_piso":item["num_piso"],
+                "num_fila":item["num_fila"],
+                "rack":[{
+                    "capacidad": item["capacidad"],
+                    "num_espcacio": item["num_espacio"]
+                }]
+
+            }]
+
+        }
+        ]
+
+
     }
 
 
