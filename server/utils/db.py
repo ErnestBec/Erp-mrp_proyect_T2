51248a8433c423275e8d832e88f8fca9b5764193
OscarRojas0510 <oscarrojasarriagax3@gmail.com>
2023-11-12 21:41:29 -0600

    inicio de rama front-end

--- server/utils/db.py
+++
@@ -1,8 +0,0 @@
-from pymongo import MongoClient
-import os
-
-db_hostname = os.getenv('DB_HOST')
-db_port = os.getenv('DB_PORT')
-conn = MongoClient(db_hostname, db_port)
-
-db_name = conn.Prueba_Tier2
