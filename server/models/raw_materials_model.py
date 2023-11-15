fd2c71e64a52a9e250bdc7ba410979daa5880155
EmmaRogel <132635424+EmmaRogel@users.noreply.github.com>
2023-11-14 22:22:49 -0600

    rawmaterial

--- server/models/raw_materials_model.py
+++ server/models/raw_materials_model.py
@@ -8,3 +8,13 @@ class raw_materials(BaseModel):
     quatity: int
     min_stock: int = 11
     max_stock: int = 100
+    id_space_Rack = int
+
+
+class updateraw_materials(BaseModel):
+    _id: Optional[str]
+    tipo: Optional[str]
+    quatity: Optional[int]
+    min_stock: Optional[int]
+    max_stock: Optional[int]
+    id_space_Rack = Optional[int]
