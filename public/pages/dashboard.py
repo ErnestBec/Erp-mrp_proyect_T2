311522056922a1bc50bb3ceb23bc7dde8fc187ed
AlePerea19 <alex_92ch@hotmail.com>
2023-12-04 18:30:23 -0600

    Cambios a la pagina de dashboard

--- public/pages/dashboard.py
+++ public/pages/dashboard.py
@@ -1,10 +1,183 @@
 from reactpy import component, html
+from components import navbar_top, navbarMenu, btnFilter, btnFilterDay, table
 from reactpy_router import link
-from components import card_graphic_ventas
-
 
 @component
 def Dashboard():
+    titulo = "Dashboard"
+
+    icono = "bi bi-clipboard-data"
+
+    opciones = [
+        "Ordenes Totales",
+        "Ordenes Pendientes",
+        "Ordenes Aprobadas",
+        "Ordenes Completadas",
+    ]
+
+    opciones2 = [
+        "Totales",
+        "Pendientes",
+        "Aprobadas",
+        "Completadas",
+    ]
+
+    columnas2 = [
+        "No",
+        "ID Orden*",
+        "Descripcion",
+        "Cantidad",
+        "Estatus",
+        "Fecha"
+    ]
+
+    datos2=[
+
+    ]
+
+    columnas = [
+        "",
+        "Cliente",
+        "ID Orden",
+        "Estado",
+        "Estado del Inventario",
+        "Fecha",
+    ]
+    datos =[
+
+
+    ]
+
+   
     return html.div(
-        
+        {"id": "app"},
+        html.div(
+            {"id": "wrapper"},
+            navbarMenu.Navbar(),
+            html.div(
+                {"id": "content-wrapper", "class_name": "d-flex flex-column"},
+                html.div(
+                    {"id": "content"},
+                    navbar_top.NavbarBusqueda(titulo),
+                    html.div(
+                        {"class_name": "container-fluid"},
+                        html.div(
+                            {
+                                "class": "card sh adow mb-4",
+                                "style": {
+                                    "background-color": "white",
+                                    "border-radius": "10px",
+                                }
+                            },
+                            html.div(
+                                {
+                                    "class": "container-fluid",
+                                    "style": {"margin-top": "5%", "margin-bottom": "5%"},
+                                }, html.div({"class":"row"},
+                                            html.div({"class":"col-auto me-auto", "style":{"color":"black"}}, 
+                                                     html.h6({"class":"display-6"}, 
+                                                             html.b("Ordenes Recientes")
+                                                            )
+                                                    )
+                                        ),
+                                    html.div({"class":"row"},
+                                              html.div({"class":"col-auto me-auto", "style":{"color":"black"}}, 
+                                                     html.p("Listado de todas las ordenes")
+                                                    )
+                                             )
+                                    
+                            ),html.div( {"class" :"card-body", "style" : {"margin-top":"0%"}},                                        #Contenedor para la grafica y botones
+                                    html.div({"class":"container-fluid"}, #Contenedor de los botones
+                                           html.div({"class":"row no-border-bottom"},
+                                                    html.div({"class":"col-auto"}, 
+                                                             html.div({"class":"btn-group"},
+                                                                    btnFilterDay.btnFilterDay(),
+                                                                    btnFilter.btnFilter(opciones)
+                                                                    )
+                                                            )
+                                                )  
+                                           
+                                    ),
+                                    html.div({"class":"table-responsive", "style" :{"margin-top" :"2%"}},
+                                                    table.Tabla(columnas,datos)
+
+                                                    )
+
+                            ),
+                        ),
+                    ),
+
+                    html.div( {"class_name": "container-fluid"},#Contenedo de la grafica
+                        html.div(
+                            {
+                                "class": "card sh adow mb-4",
+                                "style": {
+                                    "background-color": "white",
+                                    "border-radius": "10px",
+                                }
+                            },
+                            html.div({
+                                        "class": "container-fluid",
+                                        "style": {"margin-top": "5%", "margin-bottom": "5%"}}, 
+                                        html.div({"class":"row"},
+                                            html.div({"class":"col-auto me-auto", "style":{"color":"black"}}, 
+                                                     html.h6({"class":"display-6"}, 
+                                                             html.b("Graficas")
+                                                            )
+                                                    )
+                                        )                                   
+                                    ),
+                        ),
+                    ),
+
+                    html.div( #Contenedor del inventario
+                        {"class_name": "container-fluid"},
+                        html.div(
+                            {
+                                "class": "card sh adow mb-4",
+                                "style": {
+                                    "background-color": "white",
+                                    "border-radius": "10px",
+                                }
+                            },
+                            html.div(
+                                {
+                                    "class": "container-fluid",
+                                    "style": {"margin-top": "5%", "margin-bottom": "5%"},
+                                }, html.div({"class":"row"},
+                                            html.div({"class":"col-auto me-auto", "style":{"color":"black"}}, 
+                                                     html.h6({"class":"display-6"}, 
+                                                             html.b("Inventario de solicitudes")
+                                                            )
+                                                    )
+                                        ),
+                                    html.div({"class":"row"},
+                                              html.div({"class":"col-auto me-auto", "style":{"color":"black"}}, 
+                                                     html.p("Se muestra un listado de todas las solicitudes que se esperan por aprobar")
+                                                    )
+                                             )
+                                    
+                            ),html.div( {"class" :"card-body", "style" : {"margin-top":"0%"}},                                        #Contenedor para la grafica y botones
+                                    html.div({"class":"container-fluid"}, #Contenedor de los botones
+                                           html.div({"class":"row no-border-bottom"},
+                                                    html.div({"class":"col-auto"}, 
+                                                             html.div({"class":"btn-group"},
+                                                                    btnFilterDay.btnFilterDay(),
+                                                                    btnFilter.btnFilter(opciones2)
+                                                                    )
+                                                            )
+                                                )  
+                                           
+                                    ),
+                                    html.div({"class":"table-responsive", "style" :{"margin-top" :"2%"}},
+                                                    table.Tabla(columnas2,datos2)
+
+                                                    )
+
+                            ),
+                        ),
+                    )
+                ),
+            ),
+        ),
     )
--- public/pages/page_cuentas.py
+++ public/pages/page_cuentas.py
@@ -1,11 +1,34 @@
 from reactpy import component, html
-from components import navbar_top, navbarMenu, btnFilter, btnFilterDay
+from components import navbar_top, navbarMenu, btnFilter, btnFilterDay, table
 from reactpy_router import link
 
 
 @component
 
 def Page_Cuentas():
+
+    
+    opciones = [
+        "Cuentas por cobrar",
+        "Cuentas por pagar",
+
+    ]
+
+    columnas = [
+        "",
+        "No",
+        "Num. Referencia*",
+        "Importe",
+        "Estatus",
+        "Fecha de emision",
+        "Fecha de pago"
+    ]
+
+    datos = [
+
+
+    ]
+
     return html.div(
         {"id": "app"},
         html.div(
@@ -24,7 +47,7 @@ def Page_Cuentas():
                                 "style": {
                                     "background-color": "white",
                                     "border-radius": "10px",
-                                },
+                                }
                             },
                             html.div(
                                 {
@@ -44,51 +67,14 @@ def Page_Cuentas():
                                                     html.div({"class":"col-auto"}, 
                                                              html.div({"class":"btn-group"},
                                                                     btnFilterDay.btnFilterDay(),
-                                                                    btnFilter.btnFilter()
+                                                                    btnFilter.btnFilter(opciones)
                                                                     )
                                                             )
                                                 )  
                                            
                                            ),
                                            html.div({"class":"table-responsive", "style" :{"margin-top" :"2%"}},
-                                                    html.table({"className": "table", "id": "dataTable"},
-                                                               html.thead(
-                                                                    html.tr(
-                                                                        html.th({"scope":""}), 
-                                                                        html.th({"scope":"col", "children":"No"}),
-                                                                        html.th({"scope":"col", "children":"Codigo*"}),
-                                                                        html.th({"scope":"col", "children":"BOM No"}),
-                                                                        html.th({"scope":"col", "children":"Planned Qty*"}),
-                                                                        html.th({"scope":"col", "children":"Planned (Start Date) *"}),
-                                ),
-                            ),
-                         html.tbody(
-                        html.tr(
-                        html.th({"scope":"row", "children":"1"}),
-                        html.td({"children":"Mark"}),
-                        html.td({"children":"Otto"}),
-                        html.td({"children":"$300.00USD"}),
-                        html.td({"children":""}),
-                        html.td({"children":"20/11/2023"}),
-                            ),
-                            html.tr(
-                        html.th({"scope":"row", "children":"2"}),
-                        html.td({"children":"Jacob"}),
-                        html.td({"children":"Thornton"}),
-                        html.td({"children":"$100.00USD"}),
-                        html.td({"children":"Holi"}),
-                        html.td({"children":"20/11/2023"}),
-                        ),
-                        html.tr(
-                        html.th({"scope":"row", "children":"3"}),
-                        html.td({"children":"Larry the Bird"}),
-                        html.td({"children":"@twitter"}),
-                        html.td({"children":"$200.00USD"}),
-                        html.td({"children":""}),
-                        html.td({"children":"20/11/2023"}),
-                        ),
-                        ),
-                        )
+                                                    table.Tabla(columnas,datos)
 
                                                     )
 
